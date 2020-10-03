f07e93ef3bfb11d0f9692c08e5fc6898f31cabd3
ederpbj <ederpbj@gmail.com>
2020-10-03 20:37:56 -0300

    push

---
+++ app/src/components/SignInput.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import styled from 'styled-components/native'
+
+export default () => {
+      return (
+         <View>
+            <Text> textInComponent </Text>
+         </View>
+      );
+}
--- app/src/screens/SignIn/styles.js
+++ app/src/screens/SignIn/styles.js
@@ -45,4 +45,3 @@ export const SignMessageButtonTextBold = styled.Text`
     font-weight: bold;
     margin-left: 5px;
 `;
-
